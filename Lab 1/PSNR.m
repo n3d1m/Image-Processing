2d73f93ad128551bbae5a2b1a38978e6c1e7fd5d
Nedim Hodzic <nedimhodzic42@gmail.com>
2020-09-16 14:10:59 -0400

    added manual MSE function

--- Lab 1/PSNR.m
+++ Lab 1/PSNR.m
@@ -1,7 +1,7 @@
 function [PSNR_out] = PSNR(image_1,image_2)
 %Calculates the PSNR between two images
 MAX_f = 255;
-MSE = immse(image_1, image_2);
+MSE = sum((image_1(:) - image_2(:)).^2) / numel(image_1);
 PSNR_out = 10*log10(power(MAX_f,2)/MSE);
 
 %outputArg1 = inputArg1;

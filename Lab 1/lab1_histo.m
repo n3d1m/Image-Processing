de5758f66f65c2940eb17113cd6f5986a5c68f03
David Ramon Prados <davidramon3@hotmail.es>
2020-09-16 12:51:20 -0400

    Part 4 completed

---
+++ Lab 1/lab1_histo.m
@@ -0,0 +1,48 @@
+target = 'tire.tif';
+
+% Initial image
+tire = imread(target);
+histo = imhist(tire);
+figure;
+subplot(1,2,1), imshow(tire);
+subplot(1,2,2), imhist(tire);
+% Question 6 -> report
+% Question 7 -> report
+
+%Negative of the image
+tire_neg = 255 - tire;
+figure;
+subplot(1,2,1), imshow(tire_neg);
+subplot(1,2,2), imhist(tire_neg);
+
+%Verfiiy it worked
+tire_neg2 = imcomplement(tire);
+figure;
+subplot(1,2,1), imshow(tire_neg2);
+subplot(1,2,2), imhist(tire_neg2);
+%Question 8 report
+
+%Power-law transformations 
+tire_05 = double(tire).^(0.5);
+tire_05 = uint8(tire_05);
+figure;
+subplot(1,2,1), imshow(tire_05);
+subplot(1,2,2), imhist(tire_05);
+
+tire_13 = double(tire).^(1.3);
+tire_13 = uint8(tire_13);
+figure;
+subplot(1,2,1), imshow(tire_13);
+subplot(1,2,2), imhist(tire_13);
+
+% Question 9 -> report
+% Question 10 -> report
+% Question 11 -> report
+
+% Histogram equalization
+tire_eq = histeq(tire);
+figure;
+subplot(1,2,1), imshow(tire_eq);
+subplot(1,2,2), imhist(tire_eq);
+% Question 12 -> report
+% Question 13 -> report
